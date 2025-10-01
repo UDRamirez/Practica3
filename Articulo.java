8fe28da17b117f54456dd555645309689d1386fd
Sensationalforge <mikeforge@ciencias.unam.mx>
2025-09-30 18:37:38 -0600

    Create Articulo.java

---
+++ Articulo.java
@@ -0,0 +1,11 @@
+public interface Articulo{
+String getNombre();
+String getDirector();
+String getSinopsis();
+String getGenero();
+double getPrecio();
+double getDuracion(); 
+void showInformacion();
+
+
+}
