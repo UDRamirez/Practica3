7f88f7ec0d99580dbf5c0ce0d88e3e8b1b1738ae
Sensationalforge <mikeforge@ciencias.unam.mx>
2025-10-02 20:36:30 -0600

    Update Saga.java

--- Saga.java
+++ Saga.java
@@ -1,30 +1,63 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Saga implements Articulo{
+/**
+ * La clase {@code Saga} representa un conjunto de artículos (como películas o discos)
+ * agrupados bajo un mismo nombre. Implementa la interfaz {@link Articulo} y permite
+ * obtener información consolidada de todos los artículos que contiene.
+ * <p>
+ * Además, aplica un descuento en el precio total de los artículos de la saga.
+ * </p>
+ */
+public class Saga implements Articulo {
+
+    /** Nombre de la saga. */
     private String nombre;
+
+    /** Lista de artículos que conforman la saga. */
     private List<Articulo> articulos;
+
+    /** Género principal de la saga. */
     private String genero;
 
-    public Saga(String nombre, String genero){
+    /**
+     * Crea una nueva saga con el nombre y género especificados.
+     *
+     * @param nombre el nombre de la saga
+     * @param genero el género principal de la saga
+     */
+    public Saga(String nombre, String genero) {
         this.nombre = nombre;
         this.genero = genero;
         this.articulos = new ArrayList<>();
     }
 
-    public String getNombre(){
+    /**
+     * {@inheritDoc}
+     */
+    public String getNombre() {
         return this.nombre;
     }
 
-    public void addArticulo(Articulo articulo){
-
+    /**
+     * Agrega un artículo a la saga.
+     *
+     * @param articulo el artículo a añadir
+     */
+    public void addArticulo(Articulo articulo) {
         this.articulos.add(articulo);
-
     }
 
-    public String getDirector(){
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Devuelve los directores de todos los artículos de la saga en forma de lista.
+     * </p>
+     *
+     * @return una cadena con los nombres de los directores de cada artículo
+     */
+    public String getDirector() {
         String directores = "";
-        
         for (int i = 0; i < articulos.size(); i++) {
             Articulo elemento = articulos.get(i);
             directores += "Director \"" + elemento.getNombre() + "\": " + elemento.getDirector() + "\n";
@@ -32,7 +65,15 @@ public class Saga implements Articulo{
         return directores;
     }
 
-    public double getDuracion(){
+    /**
+     * {@inheritDoc}
+     * <p>
+     * La duración es la suma de las duraciones de todos los artículos.
+     * </p>
+     *
+     * @return la duración total en minutos
+     */
+    public double getDuracion() {
         double duracionTotal = 0;
         for (Articulo pelicula : articulos) {
             duracionTotal += pelicula.getDuracion();
@@ -40,13 +81,23 @@ public class Saga implements Articulo{
         return duracionTotal;
     }
 
-    public String getGenero(){
+    /**
+     * {@inheritDoc}
+     */
+    public String getGenero() {
         return genero;
     }
 
-    public String getSinopsis(){ 
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Devuelve una lista de sinopsis de todos los artículos de la saga.
+     * </p>
+     *
+     * @return una cadena con las sinopsis de cada artículo
+     */
+    public String getSinopsis() {
         String sinopsis = "";
-        
         for (int i = 0; i < articulos.size(); i++) {
             Articulo elemento = articulos.get(i);
             sinopsis += "Sinopsis \"" + elemento.getNombre() + "\": " + elemento.getSinopsis() + "\n";
@@ -54,7 +105,16 @@ public class Saga implements Articulo{
         return sinopsis;
     }
 
-    public double getPrecio(){
+    /**
+     * {@inheritDoc}
+     * <p>
+     * El precio total corresponde a la suma de los precios de todos los artículos,
+     * aplicando un descuento del 5%.
+     * </p>
+     *
+     * @return el precio total con descuento
+     */
+    public double getPrecio() {
         double precioTotal = 0;
         for (Articulo pelicula : articulos) {
             precioTotal += pelicula.getPrecio();
@@ -62,7 +122,14 @@ public class Saga implements Articulo{
         return precioTotal * 0.95;
     }
 
-    public void showInformacion(){
+    /**
+     * {@inheritDoc}
+     * <p>
+     * En el caso de una saga, muestra el nombre, la duración total,
+     * el precio con descuento, la cantidad de artículos y las sinopsis.
+     * </p>
+     */
+    public void showInformacion() {
         System.out.println("=== SAGA: " + nombre + " ===");
         System.out.println("Duración total: " + getDuracion() + " minutos");
         System.out.println("Precio con descuento: $" + getPrecio());
